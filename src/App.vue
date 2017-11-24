362f55451f1525dd7329244c6254f3e1dcdf2ce0
Billy.Liao <billfulo@gmail.com>
2017-11-24 11:13:11 +0800

    set playground envir

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <smoothyScroll></smoothyScroll>
+  </div>
+</template>
+
+<script>
+import smoothyScroll from './components/smoothy-scroll.vue'
+export default {
+  name: 'app',
+  components: {
+    smoothyScroll,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  #app{
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    background-color: #dafae5;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+</style>
---
+++ src/components/smoothy-scroll.vue
@@ -0,0 +1,119 @@
+<template>
+	<div class="smoothy" @scroll='handleScroll'>
+		<div
+			class="smoothy-item" 
+			v-for="(item, index) in items"
+			:style="{transform: 'skewY('+ skewing +'deg)', transition: skewBackSpeed+'ms'}">
+			{{index}}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+  name: 'smoothy-scroll',
+
+  data () {
+    return {
+    	scrollNow: 0,
+    	scrollNew: 0,
+    	skewing: 0,
+    	maxSkew: 3.5,
+    	skewBackSpeed: 0,
+    	isScrolling: false,
+    	items: [
+    	{
+    		"id" : 1,
+    	},
+    	{
+    		"id" : 2,
+    	},
+    	{
+    		"id" : 3,
+    	},
+    	{
+    		"id" : 4,
+    	},
+    	{
+    		"id" : 5,
+    	},
+    	{
+    		"id" : 5,
+    	},
+    	{
+    		"id" : 5,
+    	},
+    	{
+    		"id" : 5,
+    	},
+    	{
+    		"id" : 5,
+    	},    	    	    	    	    	    	    	    	
+    	],
+    }
+  },
+  methods: {
+  	handleScroll(e) {
+  		clearTimeout(this.isScrolling)
+  		this.skewBackSpeed = 0
+  		const skewLevel = this.scrollNew - this.scrollNow
+  		this.scrollNow = this.scrollNew
+  		this.scrollNew = e.target.scrollTop
+  		this.scrollNew > this.scrollNow ? 
+  			window.requestAnimationFrame(this.toSkewingDown) : window.requestAnimationFrame(this.toSkewingUp)
+  		this.isScrolling = setTimeout(()=>{
+  			this.skewBackSpeed = 1000
+  			this.skewing = 0
+  		}, 133)
+  	},
+  	toSkewingDown() {
+  		if(this.skewing < this.maxSkew){
+  			this.skewing += 0.07
+  		} else {
+  			this.skewing = this.maxSkew
+  		}  		
+  	},
+  	toSkewingUp() {
+  		if(this.skewing > -this.maxSkew){
+  			this.skewing -= 0.07
+  		} else {
+  			this.skewing = -this.maxSkew
+  		}  	
+  	},  	
+  },
+  mounted() {
+  	console.log('mounted')
+  },
+  beforeUpdate() {
+
+  },
+  updated() {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.smoothy{
+	position: relative;
+	width: 100%;
+	height: 100%;
+	padding: 0 5%;
+	overflow: scroll;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+.smoothy-item{
+	position: relative;
+	flex: 1 0 auto;
+	width: 60%;
+	height: 80%;
+	margin: 5% 0;
+	font-size: 15em;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: violet;
+}
+</style>
